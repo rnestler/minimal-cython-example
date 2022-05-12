8f2d6f13c1b82b720bbc87c0a8962839bf946f55
Raphael Nestler <raphael.nestler@renuo.ch>
2022-05-12 19:05:03 +0200

    It's only reproducible with the stable release

--- reproduce.sh
+++ reproduce.sh
@@ -4,7 +4,15 @@ if [[ ! -d .venv ]]; then
     python -m venv .venv
 fi
 source .venv/bin/activate
-pip install cython==3.0.0a10
+rm *.c *.so
+
 python -c 'import test'
+
+pip install cython==0.29.28
+cythonize -i -3 test.py
+python -c 'import test'
+
+rm *.c *.so
+pip install cython==3.0.0a10
 cythonize -i -3 test.py
 python -c 'import test'
