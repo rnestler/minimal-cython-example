cc6b0603b5e0cbf576b4f0cd1b27b8c31aef46d9
Raphael Nestler <raphael.nestler@renuo.ch>
2022-08-12 15:12:20 +0200

    Update cython version

--- reproduce.sh
+++ reproduce.sh
@@ -4,15 +4,8 @@ if [[ ! -d .venv ]]; then
     python -m venv .venv
 fi
 source .venv/bin/activate
-rm *.c *.so
-
-python -c 'import test'
-
-pip install cython==0.29.28
-cythonize -i -3 test.py
-python -c 'import test'
 
 rm *.c *.so
-pip install cython==3.0.0a10
+pip install cython==3.0.0a11
 cythonize -i -3 test.py
 python -c 'import test'
